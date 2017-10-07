5304994973e834fd2211786dffa13c3ea87674ff
mtsodf <mtsodf@gmail.com>
2017-10-06 23:12:46 -0300

    Adição de quantidade de pontos como entrada. Adição de legenda.

--- garlekin.py
+++ garlekin.py
@@ -3,16 +3,24 @@ import matplotlib.pyplot as plt
 from math import sin, pi
 import argparse 
 
-n = 200
+
+
 
 parser = argparse.ArgumentParser(description='Metodo de Garlekin')
 parser.add_argument('--entrada', type=int, default=0, help='entrada do problema')
+parser.add_argument('--pontos', type=int, default=20, help='entrada do problema')
 
 args = parser.parse_args()
 
 entrada = args.entrada
+n = args.pontos
 
 h = np.array([1.0/n]*n)
+
+#h = [0.05, 0.05, 0.1, 0.1, 0.1, 0.2, 0.1, 0.1, 0.1, 0.05, 0.05]
+#n = len(h)
+
+
 x = np.zeros(n-1)
 x[0] = h[0]
 
@@ -33,16 +41,20 @@ if entrada == 0:
     f = np.vectorize(f)
     f = f(x)
 
+    nome = "u(x) = sin(2 pi x) alpha = %f beta = %f" %( alpha, beta)
+
 elif entrada == 1:
     alpha=1; beta=0; 
 
     sol_analitica = np.apply_along_axis(lambda x: x - x*x, 0, x)
     f = np.zeros(n-1) + 2*alpha
+    nome = "u(x) = x - x*x alpha = %f beta = %f" %( alpha, beta)
     
 elif entrada == 2:
     alpha=0; beta=1;
     sol_analitica = np.apply_along_axis(lambda x: x - x*x, 0, x)
     f = np.apply_along_axis(lambda x: x - x*x, 0, x)
+    nome = "u(x) = x - x*x alpha = %f beta = %f" %( alpha, beta)
 
 
 ######################################################################
@@ -91,11 +103,13 @@ y = np.linalg.solve(K, F)
 ######################################################################
 fig, ax = plt.subplots(1,2,figsize=(15,8))
 ax[0].plot(x,y, label="Calculada")
-ax[0].plot(x, sol_analitica, label="Analitica")
+ax[0].scatter(x, sol_analitica, label="Analitica")
+ax[0].legend()
 ax[1].plot(x, np.abs(y-sol_analitica))
 
 ax[0].set_title("Solucao")
 ax[1].set_title("Erro")
+
 
 
 plt.show()
