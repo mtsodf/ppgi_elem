76442627b667e708606b331f91ee32684c7caf49
mtsodf <mtsodf@gmail.com>
2017-10-08 20:20:14 -0300

    Biblioteca de solver linear

---
+++ cpp/LinearAlgebra/Jacobi.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "../definitions.h"
+#include "Jacobi.h"
+
+
+void Jacobi(real * matrix, real *F, real tolerance, int maxiters){
+
+}
+
+
+
---
+++ cpp/LinearAlgebra/Jacobi.h
@@ -0,0 +1,6 @@
+#ifndef JACOBIH
+#define JACOBIH
+
+void Jacobi(real * matrix, real *F, real tolerance, int maxiters);
+
+#endif
