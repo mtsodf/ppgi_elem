2252a02b5d86053f987820479f987d9d72ea791a
mtsodf <mtsodf@gmail.com>
2017-10-08 19:52:20 -0300

    Cálculo da matriz global

--- cpp/Fem1d/Fem1d.cpp
+++ cpp/Fem1d/Fem1d.cpp
@@ -1,4 +1,6 @@
 #include "Fem1d.h"
+#include "../definitions.h"
+#include <stdlib.h>
 #include <cmath>
 using namespace std;
 
@@ -39,3 +41,45 @@ void LocalMatrix(real alpha, real beta, real he, real* lm){
         }
     }
 }
+
+void GlobalMatrix(int n, real alpha, real beta, real *K){
+    real * hs;
+
+    real h = 1.0/n;
+    hs = (real*) malloc(sizeof(real)*n);
+
+    for (size_t i = 0; i < n; i++)
+    {
+        hs[i] = h;
+    }
+
+    GlobalMatrix(n, alpha, beta, hs, K);
+}
+
+void GlobalMatrix(int n, real alpha, real beta, real* hs, real *K){
+
+    real lm[4];
+    int ndofs = n - 1;
+    for (size_t i = 0; i < n; i++)
+    {
+        LocalMatrix(alpha, beta, hs[i], lm);
+
+        if(i > 0 && i < ndofs){
+            K[DIM(i-1,i-1,ndofs)] += lm[0];
+            K[DIM(i-1,i+0,ndofs)] += lm[1];
+            K[DIM(i+0,i-1,ndofs)] += lm[2];
+            K[DIM(i+0,i+0,ndofs)] += lm[3];
+        }
+
+        if(i == 0){
+            K[DIM(i,i,ndofs)] += lm[3];
+        }
+
+        if(i == ndofs){
+            K[DIM(i-1,i-1,ndofs)] += lm[0];
+        }
+    }
+
+
+}
+
--- cpp/Fem1d/Fem1d.h
+++ cpp/Fem1d/Fem1d.h
@@ -9,5 +9,7 @@
 real FuncForm(real qsi, char func);
 real DFuncForm(real qsi, char func);
 void LocalMatrix(real alpha, real beta, real he, real* lm);
+void GlobalMatrix(int n, real alpha, real beta, real* h, real *K);
+void GlobalMatrix(int n, real alpha, real beta, real *K);
 
 #endif
