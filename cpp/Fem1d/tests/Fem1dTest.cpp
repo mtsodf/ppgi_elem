17ff9dd87cb54edaf7ba155803ff38ce91cf9192
mtsodf <mtsodf@gmail.com>
2017-10-08 19:53:01 -0300

    Teste da construção da matriz global

--- cpp/Fem1d/tests/Fem1dTest.cpp
+++ cpp/Fem1d/tests/Fem1dTest.cpp
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include "../Fem1d.h"
+#include "../../Utils/Utils.h"
 #include "../../ThirdParty/catch.hpp"
 
 TEST_CASE("Test Local Matrix", "[localmatrix]"){
@@ -15,3 +16,29 @@ TEST_CASE("Test Local Matrix", "[localmatrix]"){
     REQUIRE(20.06666667/2 == Approx(lm[3]).epsilon(0.0001));
 
 }
+
+
+TEST_CASE("Test Global Matrix", "[globalmatrix]"){
+    real K[9];
+
+    for (size_t i = 0; i < 9; i++)
+    {
+        K[i] = 0.0;
+    }
+
+    GlobalMatrix(4, 1, 1, K);
+
+    PrintMatrix(3, K);
+
+    REQUIRE( 8.16666667 == Approx(K[0]).epsilon(0.00001));
+    REQUIRE(-3.95833333 == Approx(K[1]).epsilon(0.00001));
+    REQUIRE( 0.00000000 == Approx(K[2]).epsilon(0.00001));
+    REQUIRE(-3.95833333 == Approx(K[3]).epsilon(0.00001));
+    REQUIRE( 8.16666667 == Approx(K[4]).epsilon(0.00001));
+    REQUIRE(-3.95833333 == Approx(K[5]).epsilon(0.00001));
+    REQUIRE( 0.00000000 == Approx(K[6]).epsilon(0.00001));
+    REQUIRE(-3.95833333 == Approx(K[7]).epsilon(0.00001));
+    REQUIRE( 8.16666667 == Approx(K[8]).epsilon(0.00001));
+}
+
+
