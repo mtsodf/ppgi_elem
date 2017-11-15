9ea3bfe03e5864aad3dadd7a22bb3de321174c72
mtsodf <mtsodf@gmail.com>
2017-11-15 13:32:23 -0200

    Removendo loops hardcoded 4 nos por cada elemento

--- python/Heat2D/heat.py
+++ python/Heat2D/heat.py
@@ -8,136 +8,9 @@ import pylab as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 from Element import *
 
-w = sqrt(3) / 3
-intPoints = [[-w, -w, 1.0], [w, -w, 1.0], [w, w, 1.0], [-w, w, 1.0]]
-
 
 DIRICHLET = 1
 NEUMANN = 2
-
-def funcform(e, n, vert):
-
-    if vert == 0:
-        return (1.0 - e) * (1.0 - n) / 4.0
-    elif vert == 1.0:
-        return (1.0 + e) * (1.0 - n) / 4.0
-    elif vert == 2:
-        return (1.0 + e) * (1.0 + n) / 4.0
-    elif vert == 3:
-        return (1.0 - e) * (1.0 + n) / 4.0
-
-    return float('nan')
-
-
-def VecFuncForm(e, n):
-    v = np.zeros((4, 1))
-
-    for i in range(4):
-        v[i] = funcform(e, n, i)
-
-    return v
-
-
-def dfuncform(e, n, vert, var):
-
-    if vert == 0:
-        if var == 0:
-            return -0.25 * (1.0 - n)
-        if var == 1:
-            return -0.25 * (1.0 - e)
-
-    if vert == 1:
-        if var == 0:
-            return 0.25 * (1.0 - n)
-        if var == 1:
-            return -0.25 * (1.0 + e)
-
-    if vert == 2:
-        if var == 0:
-            return 0.25 * (1.0 + n)
-        if var == 1:
-            return 0.25 * (1.0 + e)
-
-    if vert == 3:
-        if var == 0:
-            return -0.25 * (1.0 + n)
-        if var == 1:
-            return 0.25 * (1.0 - e)
-
-    return float('nan')
-
-
-def FormsDeriv(e, n):
-    r = np.zeros((2, 4))
-    for var in range(2):
-        for f in range(4):
-            r[var, f] = dfuncform(e, n, f, var)
-
-    return r
-
-
-def CoordM(iel, nodesCoord, lg):
-    coord = np.zeros((4, 2))
-
-    for i in range(4):
-        coord[i, :] = nodesCoord[lg[iel, i], :]
-
-    return coord
-
-
-def CalcKlocal(iel, nodesCoord, lg, Q):
-    Klocal = np.zeros((4, 4))
-
-    coord = CoordM(iel, nodesCoord, lg)
-
-    for e, n, w in intPoints:
-
-        D = FormsDeriv(e, n)
-
-        J = np.dot(D, coord)
-
-        detJ = np.linalg.det(J)
-
-        invJ = np.linalg.inv(J)
-
-        B = np.dot(invJ, D)
-
-        Bt = np.transpose(B)
-
-        Klocal = Klocal + w*np.dot(np.dot(Bt, Q), B) * detJ
-
-    return Klocal
-
-
-def CalcFlocal(iel, nodesCoord, lg, f, Q, DirichletValues):
-    n1, n2, n3, n4 = lg[iel, :]
-
-    F = np.zeros(4)
-    P = np.zeros(4)
-    fValues = np.zeros(4)
-
-    fValues[0], fValues[1], fValues[2], fValues[3] = f[n1], f[n2], f[n3], f[n4]
-
-    fValues = np.transpose(fValues)
-    coord = CoordM(iel, nodesCoord, lg)
-
-
-    P[0], P[1], P[2], P[3] = DirichletValues[n1], DirichletValues[n2], DirichletValues[n3], DirichletValues[n4]
-
-    for e, n, w in intPoints:
-        phi = VecFuncForm(e, n)
-
-        D = FormsDeriv(e, n)
-        J = np.dot(D, coord)
-        detJ = np.linalg.det(J)
-
-        F = F + np.dot(phi, np.dot(fValues, phi)) * detJ
-
-
-    # Parcela da condicao de contorno de Neumann
-    F = F - np.dot(CalcKlocal(iel, nodesCoord, lg, Q), P)
-
-    return F
 
 
 def getBoundary(i, j, nx, ny):
@@ -155,23 +28,10 @@ def getBoundary(i, j, nx, ny):
 
 def run_case(entrada, nx, ny, verbose = False, plot=False):
 
-    Qlist = []
-
     nelem = nx * ny
     nnodes = (nx + 1) * (ny + 1)
 
-    nodesCoord = np.zeros((nnodes, 2))
-
-    eq = np.zeros(nnodes, dtype=int) - 1
-
-    lg = np.zeros((nelem, 4), dtype=int)
-
-    f = np.zeros(nnodes)
     sol = []
-
-
-    NeumannValues = np.zeros(nnodes)
-    DirichletValues = np.zeros(nnodes)
 
     if entrada == 0:
         def ffunc(x, y):
@@ -249,26 +109,21 @@ def run_case(entrada, nx, ny, verbose = False, plot=False):
 
         for i in range(nx + 1):
 
-            nodesCoord[inode, 0], nodesCoord[inode, 1] = x, y
-
             nodes.append(Node(inode, x, y))
 
             if getBoundary(i, j, nx, ny) is None:
-                eq[inode] = eqCurrent
                 nodes[-1].eq = eqCurrent
                 sol.append(solfunc(x, y))
                 eqCurrent += 1
             else:
                 c = contorno[getBoundary(i, j, nx, ny)]
                 if c == NEUMANN:
-                    eq[inode] = eqCurrent
                     nodes[-1].eq = eqCurrent
                     sol.append(solfunc(x, y))
                     eqCurrent += 1
                 else:
                     nodes[inode].p = solfunc(x, y)
 
-            f[inode] = ffunc(x, y)
             nodes[inode].f = ffunc(x, y)
 
             x += dx
@@ -294,10 +149,6 @@ def run_case(entrada, nx, ny, verbose = False, plot=False):
         for i in range(nx):
 
             firstnode = i + j * (nx + 1)
-            lg[iel, 0] = firstnode
-            lg[iel, 1] = firstnode + 1
-            lg[iel, 2] = firstnode + nx + 2
-            lg[iel, 3] = firstnode + nx + 1
 
             elements.append(Quadrilateral(iel))
             elements[-1].AddNode(nodes[firstnode])
@@ -305,8 +156,6 @@ def run_case(entrada, nx, ny, verbose = False, plot=False):
             elements[-1].AddNode(nodes[firstnode + nx + 2])
             elements[-1].AddNode(nodes[firstnode + nx + 1])
 
-            # adicionando Q como identidade inicialmente
-            Qlist.append(np.identity(2))
 
             iel += 1
 
@@ -336,12 +185,12 @@ def run_case(entrada, nx, ny, verbose = False, plot=False):
         elem = elements[iel]
         Klocal = elem.CalcKlocal()
 
-        for j in range(4):
-            for i in range(4):
-                globalI = elem.nodes[i].eq
-                globalJ = elem.nodes[j].eq
+        for j, nodej in enumerate(elem.nodes):
+            for i, nodei in enumerate(elem.nodes):
+                globalI = nodei.eq
+                globalJ = nodej.eq
 
-                if globalI >= 0 and globalJ >= 0:
+                if globalI is not None and globalJ is not None:
                     K[globalI, globalJ] += Klocal[i, j]
 
     if verbose:
@@ -357,10 +206,9 @@ def run_case(entrada, nx, ny, verbose = False, plot=False):
         elem = elements[iel]
         Flocal = elem.CalcFlocal()
 
-        for i in range(4):
-            pos = eq[lg[iel, i]]
-            if(pos >= 0):
-                F[pos] += Flocal[i]
+        for i, node in enumerate(elem.nodes):
+            if node.eq is not None:
+                F[node.eq] += Flocal[i]
 
 # ***************************************************************
 #                 Adicao de Condicao de Neumann
@@ -389,13 +237,12 @@ def run_case(entrada, nx, ny, verbose = False, plot=False):
         y = []
         z = []
         z2 = []
-        for inode in range(nnodes):
-            pos = eq[inode]
-            if pos >= 0:
-                x.append(nodesCoord[inode, 0])
-                y.append(nodesCoord[inode, 1])
-                z.append(calcsol[pos])
-                z2.append(sol[pos])
+        for node in nodes:
+            if node.eq is not None:
+                x.append(node.coords[0])
+                y.append(node.coords[1])
+                z.append(calcsol[node.eq])
+                z2.append(sol[node.eq])
 
         x = np.array(x)
         y = np.array(y)
@@ -447,6 +294,8 @@ def main():
 
     residue = run_case(entrada, nx, ny, verbose)
 
+
+    print "Residuo calculado %f" % residue
 
 if __name__ == '__main__':
     main()
