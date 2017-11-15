ed27a5a2d2b7167f8adc5438087e9daa5041baf4
mtsodf <mtsodf@gmail.com>
2017-11-15 11:15:36 -0200

    Adicao de teste para fem2d

---
+++ python/Heat2D/heat_test.py
@@ -0,0 +1,26 @@
+from heat import *
+import unittest
+
+
+class TestHeat(unittest.TestCase):
+
+    def test_Dirichlet(self):
+        diff = run_case(0, 40, 40)
+
+        self.assertLess(diff, 1e-3)
+
+    def test_Dirichlet_non_hom(self):
+
+        diff = run_case(2, 40, 40)
+
+        self.assertLess(diff, 1e-3) 
+
+
+    def test_Neumann(self):
+
+        diff = run_case(1, 60, 60)
+
+        self.assertLess(diff, 1e-3) 
+
+if __name__ == '__main__':
+    unittest.main()
