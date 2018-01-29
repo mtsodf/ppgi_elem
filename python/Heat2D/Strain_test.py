5984d05b25fdfa9c3048f942eac1dcd5109e602f
mtsodf <mtsodf@gmail.com>
2018-01-29 20:37:47 -0200

    Teste na direcao y

--- python/Heat2D/Strain_test.py
+++ python/Heat2D/Strain_test.py
@@ -4,11 +4,20 @@ import unittest
 
 class TestHeat(unittest.TestCase):
 
-    def test_Dirichlet(self):
+    def test_DeslX(self):
 
         print "Test Dirichlet Homogeneo"
 
         residue = run_case(0, 20, 20, 0.0, False, False)
+
+        self.assertLess(residue, 3e-3)
+
+
+    def test_DeslY(self):
+
+        print "Test Dirichlet Homogeneo"
+
+        residue = run_case(1, 20, 20, 0.0, False, False)
 
         self.assertLess(residue, 3e-3)
 
